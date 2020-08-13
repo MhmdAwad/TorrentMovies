74a0770e23efdf2beca9f9393556c6494fa6836b
MhmdAwad <mhmdawaddd@gmail.com>
2020-08-13 13:13:55 +0200

    adding navigation component

---
+++ app/src/main/java/com/mhmdawad/torrentmovies/ui/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.mhmdawad.torrentmovies.ui
+
+import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import com.mhmdawad.torrentmovies.R
+import com.mhmdawad.torrentmovies.ui.fragments.details.DetailsFragment
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+
+        with(navHostFragment!!.childFragmentManager.fragments[0]) {
+            if (this is DetailsFragment)
+                this.onBackPressed()
+            else
+                super.onBackPressed()
+        }
+
+    }
+}
